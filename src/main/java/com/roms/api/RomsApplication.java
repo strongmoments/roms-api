43a24cef2c90e45762ffbd364dedb19339db9ff8
vikashsharma089 <vikashsharma089@gmail.com>
2022-08-31 19:20:09 +0530

    upload user password fix

--- src/main/java/com/roms/api/RomsApplication.java
+++ src/main/java/com/roms/api/RomsApplication.java
@@ -1,13 +1,20 @@
 package com.roms.api;
 
+import com.roms.api.controller.LeaveRequestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 @SpringBootApplication
 public class RomsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(RomsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RomsApplication.class, args);
