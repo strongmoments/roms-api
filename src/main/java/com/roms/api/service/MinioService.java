cfd6ed18e2c772bf9b9145cb327d202e5195945c
strongmoments <strongmoments@gmail.com>
2022-11-07 13:39:40 +0530

    wrapper image api

--- src/main/java/com/roms/api/service/MinioService.java
+++ src/main/java/com/roms/api/service/MinioService.java
@@ -80,8 +80,8 @@ public class MinioService {
         return digitalAssetService.save(digitalAsset);
     }
     private String getPreSignedUrl(String fileName,String bucketName,String type) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-      //return  new StringBuilder().append("/").append("v1/files").append("?").append("id=").append(bucketName).append("&").append("fileName=").append(fileName).append("&").append("type=").append(type).toString();
-        return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(bucketName).object(fileName).build());
+      return  new StringBuilder().append("/").append("v1/files").append("?").append("id=").append(bucketName).append("&").append("fileName=").append(fileName).append("&").append("type=").append(type).toString();
+       // return minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().method(Method.GET).bucket(bucketName).object(fileName).build());
 
     }
 
